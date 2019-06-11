970002b475cdd4135fef0a6afcda5d4b9fa09b9b
AlexJakovljevic <a.jakovljevic96@gmail.com>
2019-06-11 17:22:22 +0200

    Dodat dokaz iz deljivosti.

--- seminarski.thy
+++ seminarski.thy
@@ -172,4 +172,72 @@ case (Suc n)
   finally show ?case .
 qed
 
-end
+(* Dokazati da je broj f(n) = 2^(n+1) + 3^(2*n-1) 
+ deljiv sa 7 za sve prirodne brojeve. *)
+(* Zadatak je Primer 2. iz knjige Analiza sa algebrom 2 
+ i u knjizi se javlja greska u dokazu.*)
+
+lemma pomocna_deljivost_sa_7:
+ assumes "n \<ge> 1"
+ shows "(3::nat)^ (n * 2) = 3 * 3 ^ (n*2 - Suc 0)"
+  using [[show_types]]
+  using assms
+  by (induction n rule: nat_induct_at_least) auto
+
+lemma deljivost_sa_7:
+  fixes n::nat
+  assumes "n \<ge> 1" 
+  shows "(7::nat) dvd 2^(n+1) + 3^(2*n - 1)"
+  using assms
+proof(induction n rule: nat_induct_at_least)
+  case base
+  thus ?case 
+    by auto
+next
+  case (Suc n)
+  have "(2::nat)^(Suc n + 1) + (3::nat)^(2*(Suc n) - 1) = 2 ^ (n + 2) + 3 ^ (2*n +1)"
+    using [[show_types]]
+    by auto
+  also have "... = 2 * 2^(n + 1) + 3 * 3 * 3 ^(2*n - 1)"
+    using [[show_types]]
+    using pomocna_deljivost_sa_7
+    by (smt Groups.add_ac(2) Groups.mult_ac(1) 
+       One_nat_def Suc.hyps add_Suc_right mult.commute one_add_one plus_1_eq_Suc power_Suc)
+  also have "... = 2 * (2^(n+1) + 3^(2*n - 1) - 3^(2*n-1)) + 9 * 3 ^ (2*n - 1)"
+    by auto
+  also have "... = 2 * (2^(n+1) + 3^(2*n - 1)) - 2 * 3 ^ (2*n-1) + 9 * 3 ^(2*n-1)"
+    by auto
+  also have "... = 2 * (2^(n+1) + 3^(2*n - 1)) + 7 * 3 ^(2*n-1)"
+    by auto
+  finally show ?case
+    by (metis Suc.IH dvd_add dvd_add_times_triv_right_iff mult.commute mult_2)
+qed
+
+(* zadaci iz knjige *)
+(* zadatak 3 *)
+primrec razlomak_proizvoda_suseda :: "nat \<Rightarrow> real" where
+  "razlomak_proizvoda_suseda 0 = 0"
+| "razlomak_proizvoda_suseda (Suc n) = (1::real) / ( Suc n * ((Suc n) + 1)) + razlomak_proizvoda_suseda n"
+
+value "razlomak_proizvoda_suseda 2"
+
+lemma zbir_razlomka_proizvida_suseda: 
+  fixes n::nat
+  assumes "n \<ge> 1"
+  shows "razlomak_proizvoda_suseda n = real(n) div (n+1)"
+  using assms
+proof(induction n rule: nat_induct_at_least)
+  case base
+  then show ?case by simp
+next
+  case (Suc n)
+  then show ?case 
+  proof-
+    have "razlomak_proizvoda_suseda (Suc n) = 
+    razlomak_proizvoda_suseda n + 1/((n+1)*(n+2))"
+      by auto
+    also have "... = n/(n+1) + 1/((n+1)*(n+2))"
+      using Suc
+      by auto
+    also have "... = (n^2 + 2*n + 1)/((n+1)*(n+2))"
+      sorry
